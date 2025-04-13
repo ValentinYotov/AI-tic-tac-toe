806b1f80385ba1271f74cf8bc8a5fc670dd54904
Valentin <vyotov07@gmail.com>
2025-04-13 12:29:19 +0300

    Refactor player move functions and implement alternating turns for players

--- main.py
+++ main.py
@@ -12,23 +12,46 @@ def print_board():
     print(f'{board[6]} | {board[7]} | {board[8]}')
 
 # Ход на играч
-def player_move():
+def player_move_1():
     move = int(input("Избери позиция (1-9): ")) - 1
     if board[move] == ' ':
         board[move] = 'X'
     else:
         print("Невалиден ход!")
-        player_move()
+        player_move_1()
 
-# Случаен ход за AI
-def ai_move():
-    free_spots = [i for i in range(9) if board[i] == ' ']
-    move = random.choice(free_spots)
-    print(f"AI предлага ход: {move + 1}")
-    board[move] = 'O'
+def player_move_2():
+    move = int(input("Избери позиция (1-9): ")) - 1
+    if board[move] == ' ':
+        board[move] = 'O'
+    else:
+        print("Невалиден ход!")
+        player_move_1()
 
+def check_winner(board, player):
+    # Списък с всички печеливши комбинации (редове, колони, диагонали)
+    winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # редове
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # колони
+        [0, 4, 8], [2, 4, 6]              # диагонали
+    ]
+    
+    # Проверяваме всяка комбинация
+    for combo in winning_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] == player:
+            print(f"Играч {'1' if player == 'X' else '2'} печели!")
+            return True
+    return False
+    
+    
+
+   
 # Тест
-print_board()
-player_move()
-ai_move()
-print_board()
+for i in range(9):
+    if i % 2 == 0:
+        player_move_1()
+    else:
+        player_move_2()
+    print_board()
+    if check_winner(board, 'X'):
+        break
